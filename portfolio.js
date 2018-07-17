8d0e987f9be31a696923459603fface4ce6033c4
Michelle Garcia <garcia_michelle@att.net>
2018-07-16 21:48:12 -0500

    Added memory game to projects and fixed bottom margin/padding in projects div.

---
+++ portfolio.js
@@ -0,0 +1,10 @@
+
+$("#navbarTogglerDemo02").on('show.bs.collapse', function() {
+    $('a.nav-link').click(function() {
+        $("#navbarTogglerDemo02").collapse('hide');
+    });
+})
+
+
+
+    
